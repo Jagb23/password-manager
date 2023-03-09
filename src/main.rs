f2af1d2ce4f5ce55e7baf86edabe7b902571a4f8
Dustin Beckett <dbeckett23.dev@gmail.com>
2023-03-08 21:41:59 -0500

    added unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 
+use std::io::stdin;
 use std::ptr::null;
 use std::{collections::HashMap};
 use std::marker::PhantomData;
@@ -12,9 +13,9 @@ struct Locked;
 struct Unlocked;
 
 struct PasswordManagerEntry {
-    used_for: String,
-    password: String,
+    name: String,
     username: String,
+    password: String,
 }
 
 // PasswordManager<Locked> != PasswordManager<Unlocked>
@@ -26,8 +27,8 @@ struct PasswordManager<State = Locked> {
 }
 
 impl PasswordManager<Locked> {
-    pub fn unlock(self, master_pass: String) -> Option<PasswordManager<Unlocked>> {
-        if !argon2::verify_encoded(&self.master_pass_hash, master_pass.as_bytes()).unwrap() {
+    pub fn unlock(self, master_pass: &String) -> Option<PasswordManager<Unlocked>> {
+        if !self.is_master_password(master_pass) {
             return None
         }
 
@@ -55,23 +56,31 @@ impl PasswordManager<Unlocked> {
     }
 
     pub fn add_entry(&mut self, password_manager_entry: PasswordManagerEntry) {
-
         self.entries.push(password_manager_entry);
     }
 
-    pub fn set_master_password(&self) {
+    pub fn reset_master_password(self, current_password: &String, new_master_password: &String) -> Option<PasswordManager<Unlocked>> {
+        if !self.is_master_password(current_password) {
+            return None
+        }
 
+        let config = Config::default();
+        let salt = thread_rng().gen::<[u8;32]>();
+        let master_pass_hash = argon2::hash_encoded(new_master_password.as_bytes(), &salt, &config).unwrap();
+
+
+        Some(PasswordManager {
+            master_pass_hash,
+            entries: self.entries,
+            state: PhantomData,
+            salt: self.salt,
+        })
     }
 }
 
 impl<State> PasswordManager<State> {
-    pub fn encryption(&self, value: String) -> String {
-        todo!()
-    }
-
-
-    pub fn version(&self) -> String {
-        todo!()
+    pub fn is_master_password(&self, master_pass: &String) -> bool {
+        return argon2::verify_encoded(&self.master_pass_hash, master_pass.as_bytes()).unwrap();
     }
 }
 
@@ -91,23 +100,112 @@ impl PasswordManager {
 }
 
 fn main() {
-    let mut manager = PasswordManager::new("password123".to_owned());
+    // let manager = PasswordManager::new("password123".to_owned());
+    let input = stdin();
 
-    
-    let unlocked_manager = manager.unlock("password123".to_owned());
+    loop {
+        let manager = PasswordManager::new("123".to_owned()); // todo: move outside of this loop
+        let mut line = String::new();
 
-    match unlocked_manager {
-        Some(manager) => println!("Logged in!"),
-        None => println!("Wrong password!"),
+        println!("Enter a password: ");
+        input.read_line(&mut line); // input is read into line
+
+        println!("input: {}", line);
+        let unlocked_manager = manager.unlock(&line.trim().to_string());
+        line.clear();
+
+        match unlocked_manager {
+            Some(mut unlocked_manager) => {
+                println!("Welcome to your password manager!");
+                loop {
+                    line.clear();
+                    println!("enter a command:");
+                    input.read_line(&mut line);
+
+                    match line.trim() {
+                        "h" => println!("help"),
+                        "la" => {
+                            println!("listing passwords...");
+                            let entries = unlocked_manager.list_passwords();
+                            
+                            for entry in entries {
+                                println!("name: {} | username: {} | password {}", entry.name, entry.username, entry.password);
+                            }
+
+                        },
+                        "add" => {
+                            let mut name = String::new();
+                            println!("enter name: ");
+                            input.read_line(&mut name);
+
+                            let mut username = String::new();
+                            println!("enter username: ");
+                            input.read_line(&mut username);
+
+                            let mut password = String::new();
+                            println!("enter password: ");
+                            input.read_line(&mut password);
+
+                            let password_entry = PasswordManagerEntry {
+                                name: String::from(name.trim()),
+                                username: String::from(username.trim()),
+                                password: String::from(password.trim()), 
+                            };
+
+                            unlocked_manager.add_entry(password_entry);
+                        },
+                        "lo" => {
+                            println!("logging out...");
+                            unlocked_manager.lock();
+                            break;
+                        }
+                        _ => println!("unknown command")
+                    }
+                }
+            },
+            None => {
+                println!("Wrong password!");
+                continue;
+            },
+        } 
+
+        
+        break;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::result;
+
+    use crate::PasswordManager;
+
+    #[test]
+    fn test_unlock_invalid_password() {
+        let manager = PasswordManager::new("test_pass".to_owned());
+        let result = manager.unlock(&"wrong_password".to_string());
+
+        assert!(result.is_none());
     }
 
-    // manager.list_passwords();
-    // manager.lock();
+    #[test]
+    fn test_unlock_valid_password() {
+        let manager = PasswordManager::new("correct_password".to_owned());
+        let result = manager.unlock(&"correct_password".to_string());
 
-    // let password = b"testingareally*&^&*$#oddpasswordsdafsad";
-    // let salt = b"randomsalt";
-    // let config = Config::default();
-    // let hash = argon2::hash_encoded(password, salt, &config).unwrap();
-    // let matches = argon2::verify_encoded(&hash, password).unwrap();
-    // assert!(matches);
+        assert!(result.is_some());
+    }
+
+    #[test]
+    fn test_lock() {
+        let manager = PasswordManager::new("correct_password".to_owned());
+        let result = manager.unlock(&"correct_password".to_string());
+
+        match result {
+            Some(result) => {
+                result.lock();
+            },
+            None => { assert!(false) }
+        }
+    }
 }
